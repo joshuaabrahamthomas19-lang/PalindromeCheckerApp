ab1526950cb51fb9ccea9a3f6951f72a13a5b06b
joshuaabrahamthomas19-lang <joshuaabrahamthomas19@gmail.com>
2026-03-02 15:47:42 +0530

    UC8: Implement palindrome check using LinkedList double-ended operations

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,50 +1,49 @@
-import java.util.ArrayDeque; // Used for a high-performance double-ended queue.
-import java.util.Deque;      // The interface that allows us to work with both ends of the queue.
+import java.util.LinkedList; // We're using the LinkedList class to hold our characters.
 
 /**
  * PalindromeCheckerApp
- * UC7: Deque-Based Optimized Palindrome Checker
- * * In this version, we use a Deque (Double-Ended Queue).
- * This is a smart way to check for palindromes because we can pull
- * characters from both the "front" and "back" at the same time to see if they match.
- * @version 7.0
+ * UC8: Linked List Based Palindrome Checker
+ * * This version uses a LinkedList to store characters. Since LinkedList in Java
+ * supports double-ended operations, we can easily compare characters from
+ * both the start and end of the list.
+ * * @author Developer
+ * @version 8.0
  */
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
         // --- UC1: Welcome Message ---
         System.out.println("Welcome to the Palindrome Checker Management System");
-        System.out.println("Version : 7.0");
+        System.out.println("Version : 8.0");
         System.out.println("-------------------------------------------------");
 
-        // --- UC7: Deque Logic ---
-        // We'll test using the word "refer".
-        String input = "refer";
+        // --- UC8: Linked List Logic ---
+        // Let's test the app with the word "level".
+        String input = "level";
         System.out.println("Input: " + input);
 
-        // We create our Deque to hold the characters of the word.
-        Deque<Character> deque = new ArrayDeque<>();
+        // We create a LinkedList to store each character of the word.
+        LinkedList<Character> list = new LinkedList<>();
 
-        // We add every character from our word into the deque.
+        // Add every character of the string into the linked list.
         for (char c : input.toCharArray()) {
-            deque.addLast(c);
+            list.add(c);
         }
 
-        // We assume it's a palindrome until we find a mismatch.
+        // We assume it's a palindrome initially.
         boolean isPalindrome = true;
 
-        // We keep comparing the first and last characters until we either
-        // run out of characters or only have one left in the middle.
-        while (deque.size() > 1) {
-            // We pull one from the front and one from the back.
-            // If they aren't the same, the word isn't a palindrome.
-            if (deque.removeFirst() != deque.removeLast()) {
+        // We continue comparing characters as long as there's more than one left.
+        // We remove one from the very front and one from the very end in each step.
+        while (list.size() > 1) {
+            // If the first and last characters don't match, it's not a palindrome.
+            if (!list.removeFirst().equals(list.removeLast())) {
                 isPalindrome = false;
                 break;
             }
         }
 
-        // Output the final result to the user.
+        // Output the result of our comparison.
         System.out.println("Is Palindrome?: " + isPalindrome);
     }
 }
